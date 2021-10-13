db8c09d9c4085c43f9fa7bd83a3a97522a1951de
tatarints <tvlt2512@gmail.com>
2021-10-14 03:18:02 +0500

    Добавьте файлы проекта.

---
+++ lesson_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace lesson_4
+{
+    class lesson_4
+    {
+        public void Test()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();//создаем и запускаем таймер
+
+            Random r = new Random();
+            string[] strs = new string[10_000];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                strs[i] = ((char)r.Next('a', 'z' + 1)).ToString();
+            }
+
+            string line = "d";
+            foreach (string str in strs)
+            {
+                if (str == "d")
+                {
+                    Console.WriteLine(string.Format("Слово '{0}' содержится в массиве", line));
+                }
+            }
+
+            //останавливаем счётчик 
+            stopwatch.Stop(); //смотрим сколько миллисекунд было затрачено на выполнение 
+            Console.WriteLine($"Массив {stopwatch.ElapsedMilliseconds}");
+            stopwatch.Restart(); //перезапускаем счётчик
+
+            HashSet<string> hs = new HashSet<string>();
+            for (int i = 0; i < 10_000; i++)
+            {
+                hs.Add(i.ToString());
+            }
+
+            if (hs.Contains("d"))
+                Console.WriteLine("Строка есть");
+            else
+                Console.WriteLine("Строка отсутствует");
+
+            stopwatch.Stop(); //смотрим сколько миллисекунд было затрачено на выполнение 
+            Console.WriteLine($"HeshSet {stopwatch.ElapsedMilliseconds}");
+        }
+    }
+}
